4aecefe99f9f94a0c6200be1b9a0cf89b14081f5
Shirisha-16 <148051550+Shirisha-16@users.noreply.github.com>
2024-05-17 00:24:37 +0530

    add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let countEl=document.getElementById("count")
+let counterEl=document.getElementById("counter")
+let point=0
+function add1(){
+point=point+1
+countEl.textContent=point
+}
+function add2(){
+    point=point+2
+    countEl.textContent=point
+    }
+function add3(){
+        point=point+3
+        countEl.textContent=point
+     }
+     let pointer=0
+function sum1(){
+        pointer=pointer+1
+        counterEl.textContent=pointer
+        }
+function sum2(){
+            pointer=pointer+2
+            counterEl.textContent=pointer
+            }
+ function sum3(){
+                pointer=pointer+3
+                counterEl.textContent=pointer
+             }
